fd38fea9ba59d905cdc9137d9a20d6e8b766ebbe
ivan ortiz <ivantiz45@gmail.com>
2022-09-29 18:50:18 +0200

    Navegar por ventanas
    Asignatura de interfaces

--- Programacion/1-Evaluacion/Hotelera1/src/main/java/com/example/hotelera1/InicioController.java
+++ Programacion/1-Evaluacion/Hotelera1/src/main/java/com/example/hotelera1/InicioController.java
@@ -1,7 +1,15 @@
 package com.example.hotelera1;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class InicioController {
-    @FMXL
-    private
+    @FXML
+    private Button CrearFicha;
+    @FXML
+    public void crearFicha(ActionEvent e){
+        Ventana.cerrarVentanaActual(e);
+        Ventana.abrirVentana("CrearReserva");
+    }
 }
